b1356ad4793d5ae95f68621d95753a82c5fdfdc3
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-05-17 09:48:06 +0000

    changes to the application done at 11 and 9am

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def test_view(request):
+  return render(request, "homepage.html")
+
+
+
+## The old way of doing things
+# from django.http import HttpResponse
+
+
+# def test_view(request):
+#   name = "abdulwahab"
+#   return HttpResponse(f"""
+#   <html>
+#   <head>
+
+#   </head>
+#     <body>
+#       <h2>hello {name}!</h2>
+
+#       <p>
+#       This is our <strong>first</strong> web application!
+#       </p>
+
+#     </body>
+#   </html>
+#   """)
