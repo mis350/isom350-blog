9aef8437ec2ffd626454f76816cbaeb9f6382a17
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-05-18 10:45:22 +0000

    code from 2pm class

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+# using render (the right way)
+def test_view(request):
+  return render(request, 'ahmed.html')
+
+
+# This is the not so right way
+# from django.http import HttpResponse
+
+# # Create your views here.
+# def test_view(request):
+#   name = "jawaher"
+#   return HttpResponse(f'''
+#   <html>
+#     <body>
+    
+#     <h2>hello {name}!</h2>
+
+#   <p>
+#   this is my <strong>first</strong> web app!
+#   </p>
+
+#   <p>
+#   this is the second line
+#   </p>
+
+#     </body>
+#   </html>
+#   ''')
