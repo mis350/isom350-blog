dc2ca37d1e7dd023f81f582794cd74cb9fc7c5e5
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-06-07 07:58:54 +0000

    9am section

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
   path('greet/<str:name>/', views.greet_view),
   path('posts/', views.list_posts_view, name="list-posts"),
   path('search/title/<str:query>/',views.search_posts),
-  path('post/<slug:s>/',views.show_post, name="show-post"),
+  path('post/<slug:s>/',views.show_post, name='show-post'),
   path('create/post/',views.create_post)
 ]
